8382099256fccaaa1be2cbf02512839b16044a69
MohammadBnei <mohammadamine.banaei@pm.me>
2025-11-25 16:56:58 +0100

    okok

--- src/lib/components/TrustCard.svelte
+++ src/lib/components/TrustCard.svelte
@@ -7,24 +7,13 @@
 </script>
 
 <!-- svelte-ignore a11y_missing_attribute -->
-<a class="hover-3d my-12 mx-2 cursor-pointer w-full">
-  <div class="bg-white border border-gray-200 rounded-lg p-6 mb-6">
-    <RichText multiLine bind:content />
-    {#if $isEditing}
-      <div class="flex gap-2">
-        <SecondaryButton on:click={onMoveUp}>↑</SecondaryButton>
-        <SecondaryButton on:click={onMoveDown}>↓</SecondaryButton>
-        <SecondaryButton on:click={onDelete}>Delete</SecondaryButton>
-      </div>
-    {/if}
-  </div>
-
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</a>
+<div class="bg-white border border-gray-200 rounded-lg p-6 mb-2">
+  <RichText multiLine bind:content />
+  {#if $isEditing}
+    <div class="flex gap-2">
+      <SecondaryButton on:click={onMoveUp}>↑</SecondaryButton>
+      <SecondaryButton on:click={onMoveDown}>↓</SecondaryButton>
+      <SecondaryButton on:click={onDelete}>Delete</SecondaryButton>
+    </div>
+  {/if}
+</div>
